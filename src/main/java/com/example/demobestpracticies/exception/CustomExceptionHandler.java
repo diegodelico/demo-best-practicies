4a6d78b9b544a9cda175c651a208f80507d1ae64
Diego Salgado <diego.salgado.ext@sodexo.com>
2023-07-01 20:42:41 -0400

    Se implementa la anotacion @Order para priorizar el uso de una excepcion sobre otra, la declaracion @Order sin un parametro deja por defecto su prioridad como la ultima posible

--- src/main/java/com/example/demobestpracticies/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/demobestpracticies/exception/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.example.demobestpracticies.exception;
 
 import com.example.demobestpracticies.dto.ErrorResponseDTO;
 import feign.FeignException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -15,33 +17,44 @@ import java.time.format.DateTimeParseException;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<ErrorResponseDTO> handleValidationException(MethodArgumentNotValidException ex) {
-//        BindingResult result = ex.getBindingResult();
-//        ErrorResponseDTO errorResponse = new ErrorResponseDTO("Validation Error", "400", "", result.getFieldErrors());
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-//    }
-//
-//    @ExceptionHandler(DateTimeParseException.class)
-//    public ResponseEntity<ErrorResponseDTO> handleDateTimeParseException(DateTimeParseException ex) {
-//        ErrorResponseDTO errorResponse =
-//                new ErrorResponseDTO("Validation Error", "400", ex.getMessage(), null);
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-//    }
-//
+    @Order(1)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleValidationException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO("Validation Error", "400", "", result.getFieldErrors());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+    @Order(2)
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ErrorResponseDTO> handleDateTimeParseException(DateTimeParseException ex) {
+        ErrorResponseDTO errorResponse =
+                new ErrorResponseDTO("Validation Error", "400", ex.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+    @Order(3)
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponseDTO> handleDateTimeParseException(ConstraintViolationException ex) {
         ErrorResponseDTO errorResponse =
                 new ErrorResponseDTO("Validation Error", "400", ex.getMessage(), null);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
-//
+
+    @Order(4)
     @ExceptionHandler(FeignException.FeignClientException.class)
     public ResponseEntity<ErrorResponseDTO> handleFeignClientExceptionException(
             FeignException ex) {
         ErrorResponseDTO errorResponse =
                 new ErrorResponseDTO(ex.getMessage(), String.valueOf(ex.status()), ex.request().url(), null);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @Order
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception ex) {
+        // Manejo de excepción genérico
+        ErrorResponseDTO errorResponse =
+                new ErrorResponseDTO(ex.getMessage(), "500", ex.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
 }
